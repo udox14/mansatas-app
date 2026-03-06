d2c39fc01a3ea308438368588c868711665df236
M. Iqbal Abdul Wakil <markopolo2212@gmail.comp>
2026-03-07 00:32:27 +0700

    fix: verified kvIncrementalCache import path from node_modules

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,5 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import kvIncrementalCache from "@opennextjs/cloudflare/dist/api/kv-cache";
+import kvIncrementalCache from "@opennextjs/cloudflare/dist/api/overrides/incremental-cache/kv-incremental-cache";
 
 export default defineCloudflareConfig({
   incrementalCache: kvIncrementalCache,
