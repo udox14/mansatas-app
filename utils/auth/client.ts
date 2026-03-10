55b4186a52e787cb9a2759cf8393cb619e1dff39
M. Iqbal Abdul Wakil <markopolo2212@gmail.comp>
2026-03-10 23:43:14 +0700

    logout fix

--- utils/auth/client.ts
+++ utils/auth/client.ts
@@ -1,6 +1,5 @@
-// utils/auth/client.ts
 import { createAuthClient } from 'better-auth/client'
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || '',
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || 'https://mansatas-app.drudox.workers.dev',
 })
